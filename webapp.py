6ddcb56749c8297f625445f8e7c40dc440aecd6d
Twichher <vladickflorin@gmail.com>
2021-05-17 01:10:18 +0400

    work

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ def hello():
     return render_template('index.html', The_title='Hello World!', the_main='My favourite game')
 
 def log(name, phrase, time):
-    with open('C:\\Users\\vladi\python_prj\mywebapp\\jurnal.log', 'a') as took:
+    with open('jurnal.log', 'a') as took:
         print(name,'wrote:', phrase, 'at', time, file=took)
 
 @app.route('/res', methods=['POST'])
